0eb9ffcf43914c632023238abbb1d6cc73de94ec
ZhukX_im57@top-academy.ru <ZhukX_im57@top-academy.ru>
2025-09-10 21:13:32 +0300

    i++ done

--- ForwardList/main.cpp
+++ ForwardList/main.cpp
@@ -56,6 +56,12 @@ public:
 		Temp = Temp->pNext;
 		return *this;
 	}
+	Iterator operator++(int)
+	{
+		Iterator old = *this;
+		Temp = Temp->pNext;
+		return old;
+	}
 	bool operator==(const Iterator& other)const
 	{
 		return this->Temp == other.Temp;
@@ -488,4 +494,9 @@ void main()
 	ForwardList list = { 3,5,8,13,21 };// Перечисление значений в фигурных скобках через запятую неявно создают объект класса 'initializer list'
 	list.print();
 	for (int i : list)cout << i << tab; cout << endl;
+	cout << DELIMETER << endl;
+	for (Iterator it = list.begin(); it != list.end(); ++it)
+	{
+		cout << *it << endl;
+	}
 }
